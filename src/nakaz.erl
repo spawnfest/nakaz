001dd5ef6e982294ef0fa4decc91bee5d89676c6
Sergei Lebedev <superbobry@gmail.com>
2012-07-07 12:18:57 +0400

    Added minimal directory structure and 'rebar.config'

---
+++ src/nakaz.erl
@@ -0,0 +1,13 @@
+-module(nakaz).
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% Application callbacks
+
+start(_StartType, _StartArgs) ->
+    nakaz_sup:start_link().
+
+stop(_State) ->
+    ok.
