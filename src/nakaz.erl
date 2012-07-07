326f10237d186b86e666c85982a0f0643657a6aa
Dmitry Groshev <groshev@selectel.ru>
2012-07-07 18:11:14 +0400

    test commandline variable

--- src/nakaz.erl
+++ src/nakaz.erl
@@ -3,10 +3,19 @@
 
 %% Application callbacks
 -export([start/2, stop/1]).
+-export([start/0]).
+
+%% API
+
+start() ->
+    ok = application:start(nakaz).
 
 %% Application callbacks
 
 start(_StartType, _StartArgs) ->
+    {ok, [[ConfPath]]} = init:get_argument(nakaz),
+    {ok, Apps} = file:consult(ConfPath),
+    io:format("~p~n", [Apps]),
     nakaz_sup:start_link().
 
 stop(_State) ->
