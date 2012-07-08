06dd384ab04cfd66d1924f143aa2d26db9eca9b1
Sergei Lebedev <superbobry@gmail.com>
2012-07-09 02:44:42 +0400

    Cleaned up parse_transform-related code a bit

--- src/nakaz_pt.erl
+++ src/nakaz_pt.erl
@@ -1,38 +1,41 @@
+%%% @author Alexander Neganov <ikkeps@gmail.com>
+%%%
 %%% @doc
-%%% For now, these module just parses module records specs and inserts in it
-%%% some magic function, that returns parsed records specs. NAKAZ_ENSURE
-%%% and NAKAZ_USE uses this parsed record specs.
+%%% The current implementation simply parses all records in the transformed
+%%% module, and inserts parsed typespecs in a magic function, which can
+%%% then be callsed by {@see nakaz_core} internals to get specs for any
+%%% particular record.
 %%% @end
+
 -module(nakaz_pt).
+
 -include("nakaz_internal.hrl").
+
+%% API
 
 -export([parse_transform/2]).
 
+%% API
 
 parse_transform(Forms, _Options) ->
-    %% TODO: check that module haz special behaviour
-    %% TODO: check that required record specs is valid at compile time
-    insert_specs_getter(Forms).
+    Fun    = generate_magic_fun(Forms),
+    Export = generate_export(),
 
-%% @doc Inserts magic function that returns record specs into forms.
--spec insert_specs_getter(Forms::term()) -> NewForms::term().
-insert_specs_getter(Forms) ->
-    Func = generate_specs_getter(Forms),
-    FExport = generate_export(),
-    %% We should insert export before any function definitions
-    parse_trans:do_insert_forms(above, [FExport, Func], Forms, []).
+    %% Note(Sasha): we should insert export before any other function
+    %% definitions.
+    parse_trans:do_insert_forms(above, [Export, Fun], Forms, []).
 
-%% @doc Generate record specs getter function
--spec generate_specs_getter(Forms::term()) -> FuncForm::term().
-generate_specs_getter(Forms) ->
+%% Internal
+
+generate_magic_fun(Forms) ->
     Module = parse_trans:get_module(Forms),
-    Specs = nakaz_record_parser:extract_records_specs(Forms, Module),
-    Func = erl_syntax:function(erl_syntax:atom(?NAKAZ_MAGIC_FUN),
-                               [erl_syntax:clause(
-                                  [],
-                                  none,
-                                  [erl_syntax:abstract({ok, Specs})])]),
-    erl_syntax:revert(Func).
+    Specs  = nakaz_record_parser:extract_records_specs(Forms, Module),
+    FunDef = erl_syntax:function(erl_syntax:atom(?NAKAZ_MAGIC_FUN),
+                                 [erl_syntax:clause(
+                                    [],
+                                    none,
+                                    [erl_syntax:abstract({ok, Specs})])]),
+    erl_syntax:revert(FunDef).
 
 %% @doc Generate 'export' module attribute for magick function
 %% FIXME: better export attribute generation
--- src/nakaz_record_parser.erl
+++ src/nakaz_record_parser.erl
@@ -1,38 +1,41 @@
-%% @private
+%%% @private
 %%% @author Alexander Neganov <ikkeps@gmail.com>
 %%%
 %%% @doc
-%%% This module parses forms that cames from nakaz_pt:parse_transform
+%%% This module parses forms that cames from {@see nakaz_pt:parse_transform
 %%% and extracts all records specs. If record spec is unsupported
 %%% (in case if some fields have unsupported notation), record spec
 %%% is marked as 'unsupported' with line number and module name information.
 %%% @end
+
 -module(nakaz_record_parser).
+
 -include("nakaz_internal.hrl").
 
 -export([extract_records_specs/2]).
 
--define(BUILTIN_TYPES, [binary,integer,pos_integer,neg_integer,non_neg_integer,
-			range,number,string,nonempty_string,module,node,timeout,
-			none,byte,char,nil,list,nonempty_list,tuple,float,
-			record,boolean,atom,union]).
+-define(BUILTIN_TYPES,
+        [binary, integer, pos_integer, neg_integer, non_neg_integer,
+         range, number, string, nonempty_string, module, node, timeout,
+         none, byte, char, nil, list, nonempty_list, tuple, float,
+         record, boolean, atom, union]).
 
--define(UNSUPPORTED_TYPES, [any,port,pid,none,reference,term,maybe_improper_list,
-			    iolist, none, no_return]).
-
+%% Note(Sergei): those types cannot be covnerted from YAML values;
+%% most of them simple makes no sense in a config file.
+-define(UNSUPPORTED_TYPES,
+        [any, port, pid, none, reference, term, maybe_improper_list,
+         iolist, none, no_return]).
 
 %% @doc Extract records specifications from parse terms
--spec extract_records_specs(Forms::[term()],
-			    Module::module()) -> record_specs().
-extract_records_specs(Forms,Module) ->
-    lists:foldl(fun (F,Acc) -> handle_type(F,Acc,Module) end,
+-spec extract_records_specs([term()], module()) -> record_specs().
+extract_records_specs(Forms, Module) ->
+    lists:foldl(fun (F,Acc) -> handle_type(F, Acc, Module) end,
                 [],
                 Forms).
 
 %% @doc Handles only 'type' forms, if it is record spec then extracts it
 %%      and accumulates with Acc.
--spec handle_type(Form::term(), Acc::record_specs(), Module::module)
-		 -> Acc1::record_specs().
+-spec handle_type(term(), record_specs(), module()) -> record_specs().
 handle_type(Form, Acc, Module) ->
     case erl_syntax_lib:analyze_form(Form) of
         {attribute, {type, {type, Type}}} ->
@@ -42,60 +45,49 @@ handle_type(Form, Acc, Module) ->
     end.
 
 %% @doc Handle only records types forms
--spec handle_record(Form::term(),
-		    Acc::record_specs(),
-		    Module::module())
-		   -> Acc::record_specs().
-handle_record({{record, Name}, Fields, _Args},
-              Acc,
-	      Module) ->
-    Flds = try 
-	       [handle_field(Field, Module) || Field <- Fields] 
-	   catch
-	       throw:{unsupported_field,
-		      Form,
-		      Module} ->  {unsupported,
-				   %% FIXME: Form always has lineno as second element?
-				   element(2,Form),
-				   Module}
-    end,
+-spec handle_record(term(), record_specs(), module()) -> record_specs().
+handle_record({{record, Name}, Fields, _Args}, Acc, Module) ->
+    RecordFields =
+        try
+            [handle_field(Field, Module) || Field <- Fields]
+        catch
+            throw:{unsupported_field, Form, Module} ->
+                %% FIXME(Sashs): Does form always have a lineno as its
+                %% second element?
+                {unsupported, element(2, Form), Module}
+        end,
     %% Update ordset only if record is one of already referenced
-    [{Name, Flds} | Acc];
+    [{Name, RecordFields} | Acc];
 handle_record(_, Acc, _) ->
     Acc.
 
 %% @doc Handle record field form. Only typed_record_field supported.
--spec handle_field(Form::term(), module()) -> record_field_spec().
+-spec handle_field(term(), module()) -> record_field_spec().
 handle_field({typed_record_field,
-              {record_field,_,{atom,_,Name}}, Type}, Module) ->
+              {record_field, _, {atom, _, Name}}, Type}, Module) ->
     Field = handle_field_type(Type, Module),
     {Name, Field, undefined};
-%% Handle special case with default value
 handle_field({typed_record_field,
-              {record_field,_,{atom,_,Name}, Default}, Type}, Module) ->
-    Field = handle_value_param(Type, Module),
-    Def = handle_value_param(Default, Module),
-    {Name, Field, Def};
+              {record_field, _, {atom, _, Name}, Default}, Type},
+             Module) ->
+    %% Handle special case with default value.
+    Field   = handle_value_param(Type, Module),
+    Default = handle_value_param(Default, Module),
+    {Name, Field, Default};
 handle_field(Other, Module) ->
     throw({unsupported_field, Other, Module}).
 
 %% @doc Handle record field type. For now, only types of 'union'
 %%      supported (actually, most of field types)
-%%FIXME: Maybe there are different orders of 'undefined' atom
-%%       and other term in union
--spec handle_field_type(Form::term(), Module::module())
-		       -> nakaz_typespec().
-handle_field_type({type,_,union,
-		   [{atom,_,undefined}|
-		    Types]}, Module) ->
+-spec handle_field_type(term(), module()) -> nakaz_typespec().
+handle_field_type({type, _, union, [{atom, _, undefined}|Types]}, Module) ->
     handle_union(Types, Module);
 handle_field_type(Other, Module) ->
     throw({unsupported_field, Other, Module}).
 
 %% @doc Handle list of type forms. If there are more than one element
 %%      in list - wrap element in 'union'.
--spec handle_union(list(TypeForm::term()), Module::module())
-		  -> nakaz_typespec().
+-spec handle_union(list(term()), module()) -> nakaz_typespec().
 handle_union([Type], Module) ->
     handle_value_param(Type, Module);
 handle_union(Types, Module) ->
@@ -105,26 +97,23 @@ handle_union(Types, Module) ->
       || Type <- Types]}.
 
 %% @doc Handle actual form of field type.
-%%      Done recursively if needed.      
--spec handle_value_param(ValueParamForm::term(),
-			Module::module()) ->
-				nakaz_typespec().
-handle_value_param({remote_type, _, [{atom,_,Module},
-                                      {atom,_,Type},
-                                      Args]}, _Module) ->
-    {Module,
-     Type,
+-spec handle_value_param(term(), module()) -> nakaz_typespec().
+handle_value_param({remote_type, _,
+                    [{atom, _, Module},
+                     {atom, _, Type},
+                     Args]}, _Module) ->
+    {Module, Type,
      [handle_value_param(Arg, Module) || Arg <- Args]};
 handle_value_param({type,_,record,[]}=Form, Module) ->
     %% We do not support generic record type
     throw({unsupported_field, Form, Module});
 handle_value_param({type,_, Type, Args}=Form, Module) when is_list(Args) ->
     case lists:member(Type, ?UNSUPPORTED_TYPES) of
-	true -> throw({unsupported_field, Form, Module});
-	false ->
-	    {get_module_for_type(Type,Module),
-	     Type,
-	     [handle_value_param(Arg, Module) || Arg <- Args]}
+        true -> throw({unsupported_field, Form, Module});
+        false ->
+            {get_module_for_type(Type,Module),
+             Type,
+             [handle_value_param(Arg, Module) || Arg <- Args]}
     end;
 handle_value_param({type,_, Type, Arg}, Module) ->
     %% Handle special case when type arguments is not list
@@ -134,9 +123,9 @@ handle_value_param({type,_, Type, Arg}, Module) ->
 %% FIXME: rewrite this using erl_syntax_lib for great good.
 handle_value_param({atom,_, Atom}, _) ->
     Atom;
-handle_value_param({integer,_,Integer}, _) ->
+handle_value_param({integer, _, Integer}, _) ->
     Integer;
-handle_value_param({op,_, '-', {integer,_,Integer}}, _) ->
+handle_value_param({op,_, '-', {integer, _, Integer}}, _) ->
     %% Unary minus. Special case. Again.
     -Integer;
 handle_value_param({float,_,Float}, _) ->
@@ -145,16 +134,14 @@ handle_value_param({boolean,_,Boolean}, _) ->
     Boolean;
 handle_value_param({tuple,_,Values}, Module) ->
     list_to_tuple([handle_value_param(V, Module) || V <- Values]);
-handle_value_param({_,LineNo,_}, Module) ->
+handle_value_param({_, LineNo, _}, Module) ->
     throw({unsupported_field, LineNo, Module}).
 
 %% @doc Get module for type name. If type in builtin types, returns undefined
 %%      otherwise, returns 'undefined'.
--spec get_module_for_type(TypeName::atom(), Module::module())
-			 -> Module::module() | undefined.
+-spec get_module_for_type(atom(), module())-> module() | undefined.
 get_module_for_type(TypeName, Module) ->
     case lists:member(TypeName, ?BUILTIN_TYPES) of
-	true ->
-	    undefined;
-	false -> Module
+        true  -> undefined;
+        false -> Module
     end.
